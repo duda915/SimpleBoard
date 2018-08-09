55a50f4ca660e4a1b7a78e1412f62c4211e1e1c2
MDud <duda915@gmail.com>
2018-08-09 11:55:40 +0200

    Session factory bootstrap, listing posts, topics, creating posts,topics in TopicManager

--- src/main/java/com/mdud/simpleboard/datamodel/TopicManager.java
+++ src/main/java/com/mdud/simpleboard/datamodel/TopicManager.java
@@ -1,4 +1,107 @@
 package com.mdud.simpleboard.datamodel;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class TopicManager {
+    private SessionFactory sessionFactory;
+
+    public TopicManager(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void listTopics() {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            List topicList = session.createQuery("FROM Topic ").list();
+            Iterator iterator = topicList.iterator();
+
+            while (iterator.hasNext()) {
+                Topic topic = (Topic) iterator.next();
+                System.out.println(topic.getId() + "||" + topic.getTitle());
+            }
+
+            tx.commit();
+        } catch (HibernateException e) {
+            if(tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void listTopicPosts(int topicId) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            Topic topic = (Topic) session.createQuery("FROM Topic t WHERE t.id = :id")
+                    .setParameter("id", topicId)
+                    .getSingleResult();
+            System.out.println(topic.getTitle() + ":");
+            List<Post> posts = topic.getPosts();
+
+            for( Post p : posts) {
+                System.out.println(p.getPostContent());
+            }
+
+            tx.commit();
+        } catch (HibernateException e) {
+            if( tx != null ) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public Integer addTopic(Topic newTopic) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        Integer topicId = null;
+
+        try {
+            tx = session.beginTransaction();
+            topicId = (Integer) session.save(newTopic);
+            tx.commit();
+        } catch (HibernateException e) {
+            if(tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return topicId;
+    }
+
+    public void addTopicPost(int topicId, Post post) {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            Topic topic = (Topic) session.createQuery("FROM Topic t WHERE t.id = :id")
+                    .setParameter("id", topicId)
+                    .getSingleResult();
+
+            post.setTopic(topic);
+            topic.getPosts().add(post);
+            session.save(topic);
+            tx.commit();
+        } catch (HibernateException e) {
+            if(tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
 }
+
--- src/main/java/com/mdud/simpleboard/main/App.java
+++ src/main/java/com/mdud/simpleboard/main/App.java
@@ -1,13 +1,40 @@
 package com.mdud.simpleboard.main;
 
+import com.mdud.simpleboard.datamodel.Post;
+import com.mdud.simpleboard.datamodel.Topic;
+import com.mdud.simpleboard.datamodel.TopicManager;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static SessionFactory sessionFactory;
     public static void main( String[] args )
     {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml")
+            .build();
+
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+
+        TopicManager topicManager = new TopicManager(sessionFactory);
+
+        topicManager.listTopics();
+        topicManager.listTopicPosts(1);
+
+        sessionFactory.close();
 
     }
 }
